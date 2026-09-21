70cc60a764654ed5476d039da7a260cfcf997f85
RafaelPhenxy <rafaelsouzacarlos0@gmail.com>
2026-09-21 19:35:31 -0300

    Prendi a usar lista e comecei um exercicio mas bateu o tempo de estudo

---
+++ Listas.py
@@ -0,0 +1,38 @@
+listn = []
+for o in range(0,5):
+    listn.append(int(input('Fale um valor: ')))
+for p, va in enumerate(range[listn]):
+    if va == max(listn):
+        print(p, va)
+    if va == min(listn):
+        print(p, va)
+print(listn)
+
+
+
+val = [9, 4, 5, 0, 4, 6, 3, 2, 7, 10]
+
+print(val)
+val[4] = 1
+print(val)
+val.append(8)
+print(val)
+val.sort(reverse=True)
+print(val)
+val.pop()
+print(val)
+print(f'Valores: {len(val)}')
+val.insert(5, 0)
+print(val)
+val.remove(0)
+print(val)
+for c,v in enumerate(val):
+    print(f'Encontrei o número {v:2} na posição {c}.')
+
+a = [7, 8, 0]
+b = a
+b[1] = 6
+print(a, b)
+b = a[:]
+b[0] = 8
+print(a, b)
